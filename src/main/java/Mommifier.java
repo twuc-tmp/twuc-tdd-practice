773eb10d2c7574340ae68149223e8a6286148281
Yang Bian <yang.bian@thoughtworks.com>
2021-08-10 11:29:32 +0800

    [yang bian] fix: complete countVowels

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,10 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author yang.bian
  * @create 2021/8/10 11:07
  */
 public class Mommifier {
 
+    private List<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
     public int countVowels(String vowelsStr) {
-        return 1;
+        int count=0;
+        for (int i = 0; i < vowelsStr.length(); i++) {
+            if (vowels.contains(vowelsStr.charAt(i))){
+                count++;
+            }
+        }
+        return count;
     }
 }

bc6b4d8aea4feac6548c66fdd074584e51d651cc
Wentao Shi <wen.taoshi@Wentaos-MacBook-Pro.local>
2021-08-10 11:05:28 +0800

    test1

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MommifierTest {
+
+    @Test
+    public void should_judge_vowels_counts(){
+        String str = "ajjjjj";
+
+        Mommifier mommifier = new Mommifier();
+
+        int vowelsSize = mommifier.countVowels(str);
+
+        Assertions.assertEquals(1, vowelsSize);
+    }
+
+
 }
